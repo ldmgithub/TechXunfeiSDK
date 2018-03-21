df0d52428607bd726c94ce33de707fc450dc49ea
gocar <962722173@qq.com>
2018-03-21 17:31:13 +0800

    ini88888888888888888888888888888

--- techlib/build/generated/source/r/androidTest/debug/xunfei/tech/com/techlib/R.java
+++ techlib/build/generated/source/r/androidTest/debug/xunfei/tech/com/techlib/R.java
@@ -8,8 +8,8 @@ package xunfei.tech.com.techlib;
 
 public final class R {
     public static final class string {
-        public static final int IflytekAPP_id = 0x7f010000;
-        public static final int app_name = 0x7f010001;
-        public static final int text_begin = 0x7f010002;
+        public static final int IflytekAPP_id = 0x7f020000;
+        public static final int app_name = 0x7f020001;
+        public static final int text_begin = 0x7f020002;
     }
 }
